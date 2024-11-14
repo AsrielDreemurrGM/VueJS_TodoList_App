50718188a1e05fcaa55e1bfd13fa288b5227b7c2
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-11-13 22:50:47 -0300

    Criado Estrutura da Página

--- toDo-Vue/src/App.vue
+++ toDo-Vue/src/App.vue
@@ -3,7 +3,39 @@
 </script>
 
 <template>
-<h1>Olá Mundo!</h1>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas Tarefas</h1>
+      <p>
+        Você possui 10 tarefas pendentes
+      </p>
+    </header>
+    <form>
+      <div class="row">
+        <div class="col">
+          <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select class="form-control">
+            <option value="todas">Todas as Tarefas</option>
+            <option value="pendents">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <ul class="list-group mt-4">
+      <li class="list-group-item">
+        <input type="checkbox">
+        <label class="ms-3" for="">
+          Estudar VueJS
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
